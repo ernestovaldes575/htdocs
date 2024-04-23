27de1b505ba8eaf31553b45a04e198fa6cba2f99
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-04-23 11:59:29 -0600

    css

--- EstrPagi/Transparencia.php
+++ EstrPagi/Transparencia.php
@@ -1,15 +1,15 @@
     <section class="contenedor--transparencia">
         <div class="contenedor--transparencia--contenido">
-            <a href="/Conac/CONAC/index.php" class="shadow-xl bg-purple-700">
+            <a href="/Conac/CONAC/index.php" class="shadow-xl bg-green-1000">
                 CONAC
             </a>
-            <a href="/Conac/PAE/index.php" class="pae shadow-xl bg-purple-400">
+            <a href="/Conac/PAE/index.php" class="pae shadow-xl bg-blue-1000">
                 PAE
             </a>
-            <a href="/Conac/SRFT/index.php" class="shadow-xl bg-rose-500">
+            <a href="/Conac/SRFT/index.php" class="shadow-xl bg-yellow-1000">
                 SRFT
             </a>
-            <a href="" class="shadow-lg bg-sky-500">
+            <a href="" class="shadow-lg bg-red-1000">
                 CUENTA PÚBLICA
             </a>
         </div>
