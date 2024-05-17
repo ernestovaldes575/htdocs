4467269d860cb1653cf5367518123fac03e0cde2
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-05-17 14:53:18 -0600

    CAMBIO

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ $ClavAyun = '105';
     <section class="contenedor--titulo">
         <div class="contenedor--titulo--posicion">
             <h2 class="contenedor--titulo--posicion--contenido animate">
-                TRANSPARENCIA            </h2>
+                TRANSPARENCIA            
+            </h2>
         </div>
     </section>
     
