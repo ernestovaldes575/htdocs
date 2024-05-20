a17fec2f18d46347b6cec825d8db01f157367b72
reyesrho <reyesrho@gmail.com>
2024-05-20 13:43:11 -0600

    Conexion

---
+++ Intranet/Conexion/ConBasIntra.php
@@ -0,0 +1,11 @@
+<?php
+	$contraseña = "";
+ 
+	try{
+		$ConeBase = new PDO("mysql:host=localhost;dbname=acceso", "root", $contraseña);
+		$ConeBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$ConeBase->exec("SET CHARACTER SET utf8");
+	}catch(PDOException $error){
+		die("Conexion Fallida: ".$error->getMessage());
+	}
+?>
