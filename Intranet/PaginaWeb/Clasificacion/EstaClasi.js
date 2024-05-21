9f36b93d6da2fa672208da6020472f07f91b459e
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-21 14:04:09 -0600

    cambio nombre archivo

--- Intranet/PaginaWeb/Clasificacion/EstaClasi.js
+++ Intranet/PaginaWeb/Clasificacion/EstaClasi.js
@@ -5,17 +5,17 @@ document.addEventListener('click',
 function (event) { 
 	 if (event.target.classList.contains('Nuev')) {
 		//Cambiar nombre archivo 
-        window.location.href = 'EstaPagi.php?PaAMB01=A&PaAMB02=0';
+        window.location.href = 'EstaClasi.php?PaAMB01=A&PaAMB02=0';
       }
 else if (event.target.classList.contains('Modi')) {
         ClaEsPa = event.target.getAttribute('data-id');			
 	
 	    //Cambiar nombre archivo
-        window.location.href = 'EstaPagi.php?PaAMB01=M&PaAMB02='+ClaEsPa;
+        window.location.href = 'EstaClasi.php?PaAMB01=M&PaAMB02='+ClaEsPa;
       } 
 else if (event.target.classList.contains('Elim')) {
         ClaEsPa = event.target.getAttribute('data-id');					
         //Cambiar nombre archivo
-	    window.location.href = 'EstaPagi.php?PaAMB01=B&PaAMB02='+ClaEsPa;
+	    window.location.href = 'EstaClasi.php?PaAMB01=B&PaAMB02='+ClaEsPa;
     }    
 });
