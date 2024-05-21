e516966a8bbcd008af01a0771bb25b6059b8e4b9
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-21 13:55:36 -0600

    cambio title: clasificacion

--- Intranet/PaginaWeb/Clasificacion/EstaClasiList.php
+++ Intranet/PaginaWeb/Clasificacion/EstaClasiList.php
@@ -3,7 +3,7 @@
 <head> 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Facultades de área</title>
+	<title>Clasificacion</title>
 	<link rel="stylesheet" href="/bootstrap-icons/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="/build/css/style.css">
 </head>	
