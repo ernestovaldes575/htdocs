0c78e66986604ec091ffa4dfb5be3b6a50fce5a6
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-21 14:15:26 -0600

    cambio de tabla de bd

--- Intranet/PaginaWeb/Clasificacion/EstaClasiListSERP.php
+++ Intranet/PaginaWeb/Clasificacion/EstaClasiListSERP.php
@@ -10,8 +10,8 @@ if ( isset($_GET["Param0"]) )
 
 //Carga el registro para Consulta
 $InstSql = 	"SELECT ctplave, ctpdescrip ". //Cambiar campos
-			"FROM   pcestapagi ".			//Cambiar tabla
-			"ORDER BY CEPClave";			//Cambiar campo
+			"FROM   cctipoclas ".			//Cambiar tabla
+			"ORDER BY ctpclave";			//Cambiar campo
 			
 if ($BandMens)  
    echo '1)'.$InstSql.'<br>'; 
