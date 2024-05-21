04b918258282b6bcb4ca7a5b358b96aff7a39f67
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-21 14:06:56 -0600

    cambio de nombre archivo php

--- Intranet/PaginaWeb/Clasificacion/EstaClasiList.js
+++ Intranet/PaginaWeb/Clasificacion/EstaClasiList.js
@@ -5,7 +5,7 @@ document.addEventListener('click',
 function (event) { 
 	 if (event.target.classList.contains('Nuev')) {
 		//Cambiar nombre 
-        window.location.href = 'EstaPagi.php?PaAMB01=A&PaAMB02=0';
+        window.location.href = 'EstaClasi.php?PaAMB01=A&PaAMB02=0';
       }
 else if (event.target.classList.contains('Modi')) {
         CampBusq = event.target.getAttribute('data-CaBu');					
