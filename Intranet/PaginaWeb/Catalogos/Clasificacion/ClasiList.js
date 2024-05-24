bde03a432f984e7d400721e8391b67092fd23d23
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-24 14:57:19 -0600

    cambio de archivos

--- Intranet/PaginaWeb/Catalogos/Clasificacion/ClasiList.js
+++ Intranet/PaginaWeb/Catalogos/Clasificacion/ClasiList.js
@@ -5,18 +5,18 @@ document.addEventListener('click',
 function (event) { 
 	 if (event.target.classList.contains('Nuev')) {
 		//Cambiar nombre 
-        window.location.href = 'EstaPagi.php?PaAMB01=A&PaAMB02=0';
+        window.location.href = 'Clasi.php?PaAMB01=A&PaAMB02=0';
       }
 else if (event.target.classList.contains('Modi')) {
         CampBusq = event.target.getAttribute('data-CaBu');					
         //Cambiar nombre
-		window.location.href = 'EstaPagi.php?PaAMB01=M&PaAMB02='+CampBusq;
+		window.location.href = 'Clasi.php?PaAMB01=M&PaAMB02='+CampBusq;
       } 
 else if (event.target.classList.contains('Elim'))
 	  if (confirm('¿Estás seguro de que deseas eliminar el registro?')) 
       { CampBusq = event.target.getAttribute('data-CaBu');					
         //Cambiar nombre
-	     window.location.href = 'EstaPagi.php?PaAMB01=B&PaAMB02='+CampBusq;
+	     window.location.href = 'Clasi.php?PaAMB01=B&PaAMB02='+CampBusq;
 	  } 
        
 });
