92df708898e25b0883967fd2718839d0508a5a88
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-05-27 15:55:34 -0600

    tipo de clasificacin

--- Intranet/PaginaWeb/Catalogos/Clasificacion/ClasiListSERP.php
+++ Intranet/PaginaWeb/Catalogos/Clasificacion/ClasiListSERP.php
@@ -8,7 +8,7 @@ $BandMens = false;
 if ( isset($_GET["Param0"]) )
 	$BandMens = true;
 
-	
+//Ingresar cookie de Tipo de Clasificacion
 if ( isset($_GET["Param1"]) )
  { $TipoClas= $_GET["Param1"];
    $ArCookie = "$TipoClas|"
@@ -17,8 +17,8 @@ if ( isset($_GET["Param1"]) )
 //Carga el registro para Consulta
 $InstSql = 	"SELECT CCLClave, CCLDescripcion ". //Cambiar campos
 			"FROM   ccclasifica ".
-			"WHERE CCLTipoDocu = '$TipoClas' "			//Cambiar tabla
-			"ORDER BY CCLClave";			//Cambiar campo
+			"WHERE CCLTipoDocu = '$TipoClas' 			//Cambiar tabla
+			ORDER BY CCLClave";			//Cambiar campo
 			
 if ($BandMens)  
    echo '1)'.$InstSql.'<br>'; 
