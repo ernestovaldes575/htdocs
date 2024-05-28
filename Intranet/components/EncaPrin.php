352888a9130e3ba3fff43d756baa4efc2b5f2d91
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-05-28 14:12:51 -0600

    Intranet

--- Intranet/components/EncaPrin.php
+++ Intranet/components/EncaPrin.php
@@ -1,13 +1,15 @@
 <div class="shadow">
 	<div class="container-sm encabezado-prin">
-		<img class="logo" src="/Intranet/Encabezado/Imagen/Aytos/105/Ayt105T.png" alt="Toponimia">
+		<a href="/">
+			<img class="logo img-fluid" src="/img/logo/logveda.png" alt="Toponimia">
+		</a>
 		<div class="d-flex flex-column align-items-center justify-content-center">
 			<h1 class="fw-semibold text-center text-uppercase fs-3">
 				<span>Ayuntamiento</span> 
 				de <span>Zinacantepec</span> 
 			</h1>
 		</div>
-		<img class="logo" src="/Intranet/Encabezado/Imagen/Aytos/105/Ayt105E.png"
-		alt="OPD" class="img-fluid">
+		<img class="logo img-fluid" src="/img/logo/SIMGA01.png"
+		alt="OPD">
 	</div>
 </div>
