e7fb3d060f72a3593de6c1e84f3d61d7896499df
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-05-31 11:22:49 -0600

    Modulo02

--- EstrPagi/Modulo02.php
+++ EstrPagi/Modulo02.php
@@ -1,16 +1,16 @@
     <section class="contenedor">
         <div class="contenedor-centrar">
             <div class="contenedor-conac">
-                    <a href="https://www.zinacantepec.gob.mx/conac.php" class="">
+                    <a href="https://www.zinacantepec.gob.mx/conac.php"style="background-color:#94bf1b;">
                         CONAC
                     </a>
-                    <a href="https://www.zinacantepec.gob.mx/conac.php" class="">
+                    <a href="https://www.zinacantepec.gob.mx/conac.php" style="background-color:#00a7e1;">
                         PAE
                     </a>
-                    <a href="https://www.zinacantepec.gob.mx/conac.php" class="">
+                    <a href="https://www.zinacantepec.gob.mx/conac.php" style="background-color: #fdcb0b;">
                         SRFT
                     </a>
-                    <a href="" class="">
+                    <a href="https://www.zinacantepec.gob.mx/conac.php" style="background-color: #e40646;">
                         CUENTA PÚBLICA
                     </a>
             </div>
