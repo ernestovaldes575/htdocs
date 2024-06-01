2df713e7b0bc49ce317009567f377b2ce7859214
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-05-31 23:49:06 -0600

    Style

--- EstrPagi/Modulo01.php
+++ EstrPagi/Modulo01.php
@@ -46,7 +46,7 @@
                         ?>
                         <div class="swiper-slide">
                             <a href="<?=$InicHtml?>">
-                                <img src="<?=$ImagPagi?>" alt="" style="max-width:100%;heigth:auto;">
+                                <img src="<?=$ImagPagi?>" alt="">
                             </a>
                         </div>
                         <?php
