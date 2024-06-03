7def615d29aafbda0ced899bb6852db54ad8b21c
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-03 17:14:39 -0600

    Modulo06

--- index.php
+++ index.php
@@ -69,18 +69,7 @@
             </div>
         </div>
     </main>
-    <main class="contenedor__redes__sociales" id="facebook">
-        <div class="contenedor__principal">
-            <article>
-                <div class="fb-page" data-href="https://www.facebook.com/ManuelVilchisV" data-tabs="timeline"  data-height="610" data-small-header="false" data-adapt-contenedor-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ManuelVilchisV" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ManuelVilchisV">Manuel Vilchis Viveros</a></blockquote>
-                </div>
-            </article>
-            <article>
-                <div class="fb-page" data-href="https://www.facebook.com/AyuntamientoZinacantepecOficial" data-tabs="timeline" data-width="" data-height="610" data-small-header="false" data-adapt-contenedor-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AyuntamientoZinacantepecOficial" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AyuntamientoZinacantepecOficial">Ayuntamiento de Zinacantepec 2022-2024</a></blockquote>
-                </div>
-            </article>
-        </div>
-    </main>
+    <?php include 'EstrPagi/Modulo06.php'?>
 
     <script src="/scripts/app.js"></script>
     <script src="./swiperjs/swiper-bundle.min.js"></script>
