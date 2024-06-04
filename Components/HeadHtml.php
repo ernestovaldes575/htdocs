1f03f42a996e1fd7d347119d846404a8d13f5a1f
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-04 14:43:53 -0600

    Imagenes-Ligas

--- Components/HeadHtml.php
+++ Components/HeadHtml.php
@@ -2,25 +2,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo"$Titulo"?></title>
     <link rel="stylesheet" href="/bootstrap-icons/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="/build/css/app.css">
-    <link rel="shortcut icon" href="/img/logo/LOGO-PRINCIPAL.png"/>
-
     <link rel="stylesheet" href="../swiperjs/swiper-bundle.min.css">
-    <link rel="stylesheet" href="/layouts/Imagenes.css">
-    <link rel="stylesheet" href="/layouts/MenuCont.css">
-    <link rel="stylesheet" href="/layouts/Transparencia.css">
-    <link rel="stylesheet" href="/layouts/Main.css">
+    <link rel="shortcut icon" href="/img/logo/LOGO-PRINCIPAL.png"/>
+    <link rel="stylesheet" href="/build/css/app.css">
     
-    <link rel="stylesheet" href="/layouts/Noticias.css">
-    <link rel="stylesheet" href="/layouts/CardNot.css">
-    <link rel="stylesheet" href="/layouts/Interes.css">
-    <link rel="stylesheet" href="/layouts/Sociales.css">
-    <link rel="stylesheet" href="/layouts/Main_Sec.css">
-    <link rel="stylesheet" href="/layouts/Layer.css">
-    <link rel="stylesheet" href="/layouts/Swiper.css">
-    <link rel="stylesheet" href="/layouts/Footer.css">
-    <link rel="stylesheet" href="/layouts/Direcciones.css">
-    <link rel="stylesheet" href="/layouts/Cabildo.css">
     <link rel="stylesheet" href="/layouts/AvisoPrivacidad.css">
-    <link rel="stylesheet" href="/FrameworkCSS/Framework.css">
+    <link rel="stylesheet" href="/layouts/Cabildo.css">
+    <link rel="stylesheet" href="/layouts/Direcciones.css">
+    <link rel="stylesheet" href="/layouts/Imagenes.css">
+    <link rel="stylesheet" href="/layouts/Main_Sec.css">
+    <link rel="stylesheet" href="/layouts/Noticias.css">
+    <link rel="stylesheet" href="/layouts/MenuCont.css">
+    <link rel="stylesheet" href="/layouts/Sociales.css">
+
+
     <script src="https://unpkg.com/scrollreveal"></script>
