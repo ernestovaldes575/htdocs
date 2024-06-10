09e34edb1829a29611e8bc444ae5dc71d7c2fe5f
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-10 14:21:13 -0600

    conlogin

--- Intranet/Conexion/conlogin.php
+++ Intranet/Conexion/conlogin.php
@@ -10,7 +10,7 @@
 	// $dbname = 'acceso';
 
 	try{
-		$con = new PDO("mysql:host=localhost;dbname=$dbname", "$user", $contraseña);
+		$con = new PDO("mysql:host=localhost;dbname=$dbname", $user, $contraseña);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$con->exec("SET CHARACTER SET utf8");
 	}catch(PDOException $error){
