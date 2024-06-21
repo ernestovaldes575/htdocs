8993194cd93e01a5f080704af2ea6e2cc2261ad1
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-21 17:40:52 -0600

    Archivos de Conexion

--- Intranet/Archivos/Conexiones/ConBasComSoc.php
+++ Intranet/Archivos/Conexiones/ConBasComSoc.php
@@ -1,18 +1,14 @@
 <?php
-$ConeInEx = $_SESSION['ConeInEx'];
 	//!Local
 	$db_contraseña = '';
 	$db_usuario = 'root';
 	$db_nombre = 'comsocial';
 
-	if($ConeInEx == 'Externo'){
-		//!Servidor
-		$db_contraseña = 'NXBWSHJ4E46L';
-		$db_usuario = 'difzinac_intranet';
-		$db_nombre = 'difzinac_comsocial';
-	}
-
-
+	//!Servidor
+	// $db_contraseña = 'NXBWSHJ4E46L';
+	// $db_usuario = 'difzinac_intranet';
+	// $db_nombre = 'difzinac_comsocial';
+	
 	try{
 		$ConeBase = new PDO("mysql:host=localhost;dbname=$db_nombre", "$db_usuario", $db_contraseña);
 		$ConeBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
