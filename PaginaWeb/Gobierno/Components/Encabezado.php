19b688ff72047d3c7b8a73f320fd727f758f645c
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-24 12:50:00 -0600

    Encabazado

--- PaginaWeb/Gobierno/Components/Encabezado.php
+++ PaginaWeb/Gobierno/Components/Encabezado.php
@@ -1,6 +1,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $Titulo?></title>
+    <title><?=$Titulo?></title>
     <link rel="shortcut icon" href="/img/logo/LOGO-PRINCIPAL.png" type="image/x-icon">
     <link rel="stylesheet" href="/build/css/app.css">
     <link rel="stylesheet" href="/dest/css/style.css">
