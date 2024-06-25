9e2eddeb0f5e79ef53c5a280a0c9a1e546b0e520
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-25 13:04:02 -0600

    Modificacion imagenes

--- PaginaWeb/EstrPagi/Modulo01.php
+++ PaginaWeb/EstrPagi/Modulo01.php
@@ -59,13 +59,13 @@
         <article class="article__exp item-2">
             <div class="gallery">
                 <a href="https://dif.zinacantepec.gob.mx/">
-                    <img src="../img/Imagenes_Exp/DIF_Edificio.png" alt="DIF">
+                    <img src="/PaginaWeb/img/DIF_Edificio.webp" alt="DIF">
                 </a>
                 <a href="https://opdapas.zinacantepec.gob.mx/">
-                    <img src="../img/Imagenes_Exp/OPDAPAS_EDIF.jpg" alt="OPDAPAS">
+                    <img src="/PaginaWeb/img/OPDAPAS_EDIF.webp" alt="OPDAPAS">
                 </a>
                 <a href="https://imcufidez.zinacantepec.gob.mx/">
-                    <img src="../img/Imagenes_Exp/IMCUFIDEZ_ZIN.jpg" alt="IMCUFIDEZ">
+                    <img src="/PaginaWeb/img/IMCUFIDEZ_ZIN.webp" alt="IMCUFIDEZ">
                 </a>
             </div>
         </article>
