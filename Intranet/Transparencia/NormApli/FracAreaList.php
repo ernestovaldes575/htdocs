aab06a322bf44140644e45e5cc42be064f2fe0db
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-06-27 17:52:25 -0600

    Modificacion en la carpeta Css dentro de intranet

--- Intranet/Transparencia/NormApli/FracAreaList.php
+++ Intranet/Transparencia/NormApli/FracAreaList.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head> 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Facultades de área</title>
-	<link rel="stylesheet" href="/bootstrap-icons/font/bootstrap-icons.min.css">
-	<link rel="stylesheet" href="/Intranet/Css/style.css">
+	<?php
+		include '../NormApli/Componente/LigasHTML.php';
+	?>
 </head>
 <body>
 <script src="FracAreaList.js"></script>
@@ -19,66 +17,70 @@
 	include 'FracAreaListSERP.php';
 ?>	
 <!--encabezado--> 
-<div class="container table-responsive">
-	<table width="70%" class="ListInfo tabla">
-		<tr>
-			<td width="5%"></td>
-			<td colspan="2"><a href="FracLista.php" 
-					class="btn-Regresar">Regresar</a></td>
-		</tr>
-		<tr>
-			<th>Clave</th>
-			<th width="72%">Subinciso</th>
-			<th width="23%"></th>
-		</tr>
-		<?php 
-		  foreach($ResSql1 as $RegiTabl){
-			  $VC03 = $RegiTabl[0];	
-			  $VC04 = $RegiTabl[1];
-			  $VC05 = $RegiTabl[2];
-			  
-		?>
-		<tr>
-			<td><?=$VC04?></td>
-			<td><?=$VC05?></td>
-			<td>			 
-				 <i class="bi bi-file-arrow-down btn-Modificar ElimArea "
-				 data-ConsUnid='<?= $VC03?>' 	
-				 title="3er Trimestre"></i>
-			</td>
-		</tr>
-		<?php	} ?> 
-	</table>
-	<table width="70%" class="ListInfo tabla">
-		<tr>
-			<td width="5%"></td>
-			<td colspan="2"><a href="FracLista.php" 
-					class="btn-Regresar">Regresar</a></td>
-		</tr>
-		<tr>
-			<th>Clave</th>
-			<th width="72%">Subinciso</th>
-			<th width="23%"></th>
-		</tr>
-		<?php 
-		  foreach($ResSql2 as $RegiTabl){
-			  $VC03 = $RegiTabl[0];	
-			  $VC04 = $RegiTabl[1];
-			  $VC05 = $RegiTabl[2];
-			  
-		?>
-		<tr>
-			<td><?=$VC04?></td>
-			<td><?=$VC05?></td>
-			<td>			 
-				 <i class="bi bi-file-arrow-down btn-Modificar AltaArea"
-				 data-ConsUnid='<?= $VC03?>' 	
-				 title="3er Trimestre"></i>
-			</td>
-		</tr>
-		<?php	} ?> 
-	</table>
-
+	<div class="container table-responsive">
+		<table class="ListInfo tabla">
+			<tr>
+				<td width="5%"></td>
+				<td colspan="2" class="text-end">
+					<a href="FracLista.php" class="btn-Regresar">
+						Regresar
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<th width="10%">Clave</th>
+				<th width="70%">Subinciso</th>
+				<th width="20%"></th>
+			</tr>
+			<?php 
+				foreach($ResSql1 as $RegiTabl){
+						$VC03 = $RegiTabl[0];	
+						$VC04 = $RegiTabl[1];
+						$VC05 = $RegiTabl[2];
+			?>
+			<tr>
+				<td><?=$VC04?></td>
+				<td><?=$VC05?></td>
+				<td class="text-center">			 
+					<i class="bi bi-file-arrow-down btn-Modificar ElimArea "
+					data-ConsUnid='<?=$VC03?>' 	
+					title="3er Trimestre"></i>
+				</td>
+			</tr>
+			<?php } ?> 
+		</table>
+		<table class="ListInfo tabla">
+			<tr>
+				<td width="5%"></td>
+				<td colspan="2" class="text-end">
+					<a href="FracLista.php" class="btn-Regresar">
+						Regresar
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<th>Clave</th>
+				<th width="72%">Subinciso</th>
+				<th width="23%"></th>
+			</tr>
+			<?php 
+				foreach($ResSql2 as $RegiTabl){
+						$VC03 = $RegiTabl[0];	
+						$VC04 = $RegiTabl[1];
+						$VC05 = $RegiTabl[2];
+			?>
+			<tr>
+				<td><?=$VC04?></td>
+				<td><?=$VC05?></td>
+				<td>			 
+					<i 	class="bi bi-file-arrow-down btn-Modificar AltaArea"
+						data-ConsUnid='<?= $VC03?>' 	
+						title="3er Trimestre">
+					</i>
+				</td>
+			</tr>
+			<?php } ?> 
+		</table>
 	</div>	
 
 <?php
