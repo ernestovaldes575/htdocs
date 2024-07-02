d15de72f5970293a1e4fab780428b31379f80c23
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-07-02 13:06:39 -0600

    BD

--- BD/acceso.sql
+++ BD/acceso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2024 a las 21:04:36
+-- Tiempo de generación: 02-07-2024 a las 21:05:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
