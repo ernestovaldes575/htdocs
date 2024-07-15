9f2d2eb7735f9dad7b96cd08eea719f613cb18d0
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-07-15 17:43:19 -0600

    DataTable con bootstrap

--- Intranet/Transparencia/NormApli/Componente/LigasHTML.php
+++ Intranet/Transparencia/NormApli/Componente/LigasHTML.php
@@ -5,3 +5,4 @@
 <link rel="stylesheet" href="/bootstrap-icons/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="/Intranet/CSS/Estilos/style.css">
 <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="/Intranet/Transparencia/">
