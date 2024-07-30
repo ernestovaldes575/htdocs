da92d62553cf142610847d94ee7a9ae35bb8683d
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-07-30 11:44:03 -0600

    Ligas FracLista

--- Intranet/Transparencia/NormApli/FracLista.php
+++ Intranet/Transparencia/NormApli/FracLista.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
 	<?php
 		include '../NormApli/Componente/LigasHTML.php';
 	?>
