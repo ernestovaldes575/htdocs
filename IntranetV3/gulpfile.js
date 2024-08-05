737c6349a2a62773a3475598f1544b0d1758da23
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-08-05 14:18:52 -0600

    Importando boostrap dentro de Gulp

---
+++ IntranetV3/gulpfile.js
@@ -0,0 +1,9 @@
+//?Importacion de las librerias de bootstrap y Gulp-Sass
+const{src,dest,watch,series} = require('gulp');
+const sass = require('gulp-postcss')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+function css(done){
+    src('')
+}
