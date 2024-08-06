5aaff15ef947a58b2461a11681bb63a5e8b9ee9e
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-08-06 11:11:18 -0600

    fw-bold Ayun-Zina

--- IntranetV3/includes/Encabezado.php
+++ IntranetV3/includes/Encabezado.php
@@ -3,7 +3,7 @@
 		<a href="/" class="logo-mur">
 			<img src="/Intranet/img/LogoMurc.webp" alt="LogoMurc" class="img-fluid">
 		</a>
-		<h1 class="fs-3 fw-semibold">
+		<h1 class="fs-3 fw-bold">
 			Ayuntamiento de Zinacantepec
 		</h1>
 		<img src="/Intranet/img/LogoEscu.webp" alt="LogoEscu" class="img-fluid LogoEscu">
