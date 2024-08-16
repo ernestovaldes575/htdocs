50db696433011c65103dd41673fcfb3b1c1bc2ea
Dancobos <dreyesc016@alumno.uaemex.mx>
2024-08-16 16:54:50 -0600

    fracc 02

--- Intranet/Transparencia/registros.php
+++ Intranet/Transparencia/registros.php
@@ -4,26 +4,47 @@
     <title>Registros</title>
 </head>
 <body>
-    <form method="post" action="procesar.php">
-        <label for="numregis">Número de personas a registrar:</label>
-        <input type="number" id="numregis" name="numregis">
-        <button type="button" onclick="agregar()">Agregar</button>
-        <div id="campos"></div>
-    </form>
-
-    <?php
+        <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $numregis = $_POST['numregis'];
-
-    // Procesar los datos de cada persona
-    for ($i = 1; $i <= $numregis; $i++) {
-        $VC06 = $_POST['VC06'.$i];
-        // ... otros campos
-        
-        // Aquí puedes guardar los datos en una base de datos, un archivo, etc.
-        echo "Nombre persona $i: $VC06<br>";
-    }
-}
-?>
-</body>
-</html>
+            // Procesar los datos del formulario (si ya se ha enviado)
+            $numPersonas = $_POST['numPersonas'];
+    
+            echo "<table border='1'>";
+            echo "<tr>
+                    <th>No.</th>
+                    <th>Fecha Inicio</th>
+                    <th>Fecha de Término</th>
+                    <th>Hipervínculo</th>
+                    <th>Área Responsable</th>
+                    <th>Fecha de Actualización</th>
+                    <th>Fecha de Valdiación</th>
+                    <th>Nota</th>
+                </tr>";
+            for ($i = 0; $i < $numPersonas; $i++) {
+                echo "<tr>";
+                echo "<td><input type='number' name='C05' id='VC05' value='<?=$VC05?>' 
+						class='form-control' placeholder='Descripción'></td>";
+                echo "<td><input type='date' name='C06' id='VC06'></td>";
+                echo "<td><input type='date' name='C07' id='VC07'></td>";
+                echo "<td><input type='number' name='C08' id='VC08' value='<?=$VC05?>' 
+                class='form-control' placeholder='Descripción'></td>";
+                echo "<td><input type='date' name='C09' id='VC09'></td>";
+                echo "<td><input type='date' name='C10' id='VC10'></td>";
+                echo "<td><input type='date' name='C11' id='VC11'></td>";
+                echo "<td><input type='text' name='C12' id='VC12'></td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo "<button type='submit'>Registrar</button>";
+        } else {
+            // Mostrar el formulario inicial
+            ?>
+            <form method="post">
+                Número de personas a registrar: <input type="number" name="numPersonas" required>
+                <button type="submit">Llenar formulario</button>
+            </form>
+            <?php
+        }
+        ?>
+    </body>
+    </html>
