342a859f0ccf0958680371bc67d0f6c4b92944c4
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-08-30 17:28:07 -0600

    Conexion Entrega

---
+++ Intranet/Conexion/ConBasEntrega.php
@@ -0,0 +1,19 @@
+<?php
+
+	//!Local
+	$db_contraseña = '';
+	$db_usuario = 'root';
+	$db_nombre = 'entrega';
+
+	//!Servidor
+	// $db_contraseña = 'E9TQE4QXOP3A';
+	// $db_usuario = 'difzinac_intranet';
+	// $db_nombre = 'difzinac_transparencia';
+	
+	try{
+		$ConeBase = new PDO("mysql:host=localhost;dbname=$db_nombre", "$db_usuario", $db_contraseña);
+		$ConeBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$ConeBase->exec("SET CHARACTER SET utf8");
+	}catch(PDOException $error){
+		die("Conexion Fallida: ".$error->getMessage());
+	}
